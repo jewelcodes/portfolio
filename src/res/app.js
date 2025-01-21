d699896410dc4912fbb2da69352e6912f97c394e
jewelcodes <omarmelghoul01@gmail.com>
2025-01-21 14:31:44 -0500

    updated email

--- src/res/app.js
+++ src/res/app.js
@@ -16,13 +16,13 @@ const artText = "\
     * Font: [Agave](https://b.agaric.net/page/agave), distributed under the [MIT license](https://github.com/blobject/agave/blob/master/LICENSE)";
 
 const contactText = "\
-    * Email: [omarmelghoul01@gmail.com](mailto:omarmelghoul01@gmail.com)\n\
+    * Email: [oelghoul@bu.edu](mailto:oelghoul@bu.edu)\n\
     * GitHub: [jewelcodes](https://github.com/jewelcodes)\n\
     * [LinkedIn profile](https://linkedin.com/in/omarmelghoul/)";
 
 const mainText = "\
     Hi, I'm Omar Elghoul, aka Jewel c:\n\n \
-    I'm a junior software engineer from the [land of the pyramids](https://en.wikipedia.org/wiki/Cairo) currently residing in [Boston](https://en.wikipedia.org/wiki/Boston). \
+    I'm a software engineer from the [land of the pyramids](https://en.wikipedia.org/wiki/Cairo) currently living in [Boston](https://en.wikipedia.org/wiki/Boston). \
     I'm pursuing a master's in computer science & I've had a passion for coding for as far back as I can remember. Some of my work is showcased here.\n\n \
     Everything you see here is powered by a custom UI framework and blogging system made with love [entirely in JavaScript](https://github.com/jewelcodes/portfolio).";
 
